0dbaa1f17fd919dbe5eca89b27e0d3516e0240b3
Leo Gavin <leogavin123@outlook.com>
2024-02-21 10:13:35 +0530

    feat: verify status

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,11 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 import { ClsService } from 'nestjs-cls';
-import { AppClsStore } from 'src/Types/user.types';
+import { AppClsStore, UserStatus } from 'src/Types/user.types';
 import { UserService } from 'src/user/user.service';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
-
   constructor(
     private readonly clsService: ClsService,
     private userService: UserService,
@@ -18,11 +16,19 @@ export class AuthService {
     if (!context || !context.user) {
       throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
     }
-    return this.userService.findOne({userId: context.user.id});
+    return this.userService.findOne({ userId: context.user.id });
   }
 
   create(createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
+  async verify(email: string) {
+    const user = await this.userService.findOne({ email: email });
+    if (!user) {
+      throw new HttpException('Email is not registered', HttpStatus.NOT_FOUND);
+    }
+    user.status = UserStatus.Verified;
+    user.save();
+  }
 }
