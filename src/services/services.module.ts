0bfe66e31621ede2e89eed8c6ee2b104321cc987
Leo Gavin <leogavin123@outlook.com>
2024-03-05 15:22:35 +0530

    feat: categories validation

--- src/services/services.module.ts
+++ src/services/services.module.ts
@@ -4,11 +4,15 @@ import { ServicesController } from './services.controller';
 import { ClsModule } from 'nestjs-cls';
 import { Service, ServiceSchema } from './entities/service.entity';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from 'src/user/user.module';
+import { CategoriesModule } from 'src/categories/categories.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Service.name, schema: ServiceSchema }]),
     ClsModule,
+    UserModule,
+    CategoriesModule,
   ],
   controllers: [ServicesController],
   providers: [ServicesService],
