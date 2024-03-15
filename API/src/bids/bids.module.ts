7e24d3cbf624d830b4d08da63c1ea32a98cd5120
Leo Gavin <leogavin123@outlook.com>
2024-03-15 13:58:48 +0530

    feat:Bid create

--- API/src/bids/bids.module.ts
+++ API/src/bids/bids.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { BidsService } from './bids.service';
 import { BidsController } from './bids.controller';
+import { Bid, BidSchema } from './entities/bid.entity';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ClsModule } from 'nestjs-cls';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Bid.name, schema: BidSchema }]),
+    ClsModule,
+  ],
   controllers: [BidsController],
   providers: [BidsService],
+  exports: [BidsService],
 })
 export class BidsModule {}
--- API/src/bids/entities/bid.entity.ts
+++ API/src/bids/entities/bid.entity.ts
@@ -1,1 +1,32 @@
-export class Bid {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, Min } from 'class-validator';
+import { Service } from 'src/services/entities/service.entity';
+import * as mongoose from 'mongoose';
+
+export type BidDocument = Bid & Document;
+
+@Schema({ timestamps: true })
+export class Bid {
+  @Prop({ type: String, required: true })
+  @IsNotEmpty()
+  customer: string;
+
+  @ApiProperty({ description: 'Service', type: Service })
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Service',
+    required: true,
+  })
+  @IsNotEmpty()
+  service: Service;
+
+  @ApiProperty({ description: 'Bid price', example: 100.5 })
+  @Prop({ required: true })
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
+  price: number;
+}
+
+export const BidSchema = SchemaFactory.createForClass(Bid);
