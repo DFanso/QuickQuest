7226b9cadc3b2d45a97e7112b7fd8fc7e68f8ebb
Leo Gavin <leogavin123@outlook.com>
2024-03-29 13:10:48 +0530

    feat: job cancelation

--- API/src/jobs/jobs.service.ts
+++ API/src/jobs/jobs.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateJobDto } from './dto/create-job.dto';
 import { UpdateJobDto } from './dto/update-job.dto';
 import { Offer } from 'src/offers/entities/offer.entity';
@@ -8,12 +12,14 @@ import { JobStatus } from 'src/Types/jobs.types';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { PaypalService } from 'src/paypal/paypal.service';
+import { EmailService } from 'src/email/email.service';
 
 @Injectable()
 export class JobsService {
   constructor(
     @InjectModel(Job.name) private jobModel: Model<JobDocument>,
     private paypalService: PaypalService,
+    private readonly emailService: EmailService,
   ) {}
 
   async create(offer: Offer, customer: User): Promise<Job> {
@@ -83,6 +89,86 @@ export class JobsService {
       })
       .populate('service')
       .exec();
+  }
+
+  async cancelOrder(id: string): Promise<void> {
+    const job = await this.jobModel.findById(id);
+
+    if (!job) {
+      throw new NotFoundException('Job not found');
+    }
+
+    const currentDate = new Date();
+    const orderedDate = job.orderedDate;
+    const oneDayInMilliseconds = 24 * 60 * 60 * 1000; // 1 day in milliseconds
+
+    if (currentDate.getTime() - orderedDate.getTime() > oneDayInMilliseconds) {
+      throw new BadRequestException(
+        'Cannot cancel the order. The job is more than one day old.',
+      );
+    }
+
+    if (job.jobStatus !== 'PENDING') {
+      throw new BadRequestException(
+        'Cannot cancel the order. The job is not in the PENDING status.',
+      );
+    }
+
+    job.jobStatus = JobStatus.Cancelled;
+    
+
+    const cancelledJob = await this.findOne(id);
+    const refundAmount = cancelledJob.price * 0.95;
+    // Render cancellation email content
+    const emailContent = await this.emailService.renderTemplate(
+      'customer-job-cancellation.hbs',
+      {
+        customerName:
+          cancelledJob.customer.firstName +
+          ' ' +
+          cancelledJob.customer.lastName,
+        orderID: id,
+        serviceName: cancelledJob.service.name,
+        description: cancelledJob.service.description,
+        price: cancelledJob.price,
+        orderedDate: cancelledJob.orderedDate.toISOString(),
+        cancellationReason: 'Customer requested cancellation',
+        refundAmount: refundAmount,
+      },
+    );
+
+    const workerEmailContent = await this.emailService.renderTemplate(
+      'worker-job-cancellation.hbs',
+      {
+        workerName:
+          cancelledJob.worker.firstName + ' ' + cancelledJob.worker.lastName,
+        orderID: id,
+        serviceName: cancelledJob.service.name,
+        description: cancelledJob.service.description,
+        price: cancelledJob.price,
+        orderedDate: cancelledJob.orderedDate.toISOString(),
+        customerName:
+          cancelledJob.customer.firstName +
+          ' ' +
+          cancelledJob.customer.lastName,
+        cancellationReason: 'Customer requested cancellation',
+      },
+    );
+
+    // Send cancellation email
+    await this.emailService.sendEmail(
+      [cancelledJob.customer.email],
+      'Order Cancellation Confirmation',
+      emailContent,
+    );
+
+    await this.emailService.sendEmail(
+      [cancelledJob.worker.email],
+      'Job Cancellation Notification',
+      workerEmailContent,
+    );
+
+    await job.save();
   }
 
   update(id: number, updateJobDto: UpdateJobDto) {
