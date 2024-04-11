c8f02a78fe6477ed0063e8c94e8d988a3bc8d9d4
Leo Gavin <leogavin123@outlook.com>
2024-04-11 21:25:24 +0530

    feat: rec population done

--- API/src/recommendations/recommendations.module.ts
+++ API/src/recommendations/recommendations.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { RecommendationsService } from './recommendations.service';
 import { RecommendationsController } from './recommendations.controller';
+import { HttpModule } from '@nestjs/axios';
+import { UserModule } from 'src/user/user.module';
+import { ClsModule } from 'nestjs-cls';
 
 @Module({
+  imports: [HttpModule, UserModule, ClsModule],
   controllers: [RecommendationsController],
   providers: [RecommendationsService],
+  exports: [RecommendationsService],
 })
 export class RecommendationsModule {}
